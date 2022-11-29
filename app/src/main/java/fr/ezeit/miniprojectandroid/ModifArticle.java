367b8edd41cde525fa9867e9d026865851e3d45e
sylvain <s.crochon@lyon.ort.asso.fr>
2022-11-29 10:15:14 +0100

    Add Modifier_Article.xml et Liste_Article.xml

---
+++ app/src/main/java/fr/ezeit/miniprojectandroid/ModifArticle.java
@@ -0,0 +1,14 @@
+package fr.ezeit.miniprojectandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModifArticle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modif_article);
+    }
+}
---
+++ app/src/main/java/fr/ezeit/miniprojectandroid/listeArticle.java
@@ -0,0 +1,14 @@
+package fr.ezeit.miniprojectandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listeArticle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste_article);
+    }
+}
